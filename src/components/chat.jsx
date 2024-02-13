76d3e9c85e2b637a5edefc73a6d781b8884c9cc8
arjith_arch2 <arjith_arch2@gmail.com>
2024-02-14 01:13:56 +0530

    added chat window

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,21 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 export function Chat_console(){
-	const [data, setdata] = useState("Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.")
-//	useEffect(()=>{
-//		axios.post("http://127.0.0.1:8000/chat/ttot",{text:"hi how are you"})
-//			.then((r)=>{
-//
-//				console.log(r.data)
-//				setdata(r.data.text)})
-//
-//	},[])
+	const [data, setdata] = useState([])
+	const inputref=useRef("")
+	function get_ttot(){
+		let input_text=inputref.current.value
+		axios.post("http://127.0.0.1:8000/chat/ttot",{text:input_text})
+			.then((r)=>{
+				//implement proper way to handle req errors
+				if(r.status!=200){
+					alert(r.data)
+					console.log("error")
+
+				}
+
+				setdata([...data,[input_text,r.data.text]])})
+
+	}
+	console.log(data)
+
 	return(
+		<div className="chat_page">
 		<div className="chat_container">
-		<h1>{data}</h1>
+		<div className="content">
+
+		{data.map((e)=>{
+			return(
+				<div>
+				<p><i>{e[0]}</i></p>
+				<p>{e[1]}</p>
+
+				</div>	
+			)
+
+
+		})}
+		</div>	
 		<div className="input">
 
-		<textarea rows="10" cols="90"></textarea>
+		<textarea rows="10"  ref={inputref}  cols="90"></textarea>
+		<button onClick={get_ttot} type="">submit</button>
+		</div>
 		</div>
 
 
